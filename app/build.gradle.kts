ac59442f3148bc4a8dc71dea53327cfa0b5055ce
Himanshu Chauhan <hichauhan.in@gmail.com>
2026-09-12 21:42:44 +0530

    build: bump version to 16.0
    Update versionCode to 16 and versionName to 16.0 for the new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "in.hichauhan.mynotes"
     minSdk = 24
     targetSdk = 36
-    versionCode = 15
-    versionName = "2.0.1.5"
+    versionCode = 16
+    versionName = "16.0"
     buildConfigField("boolean", "EXTERNAL_SUPPORT_ENABLED", "true")
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
